c95c9f84b69e428bee884bd730c977da3401524d
wayline <44699179+wayline89@users.noreply.github.com>
2026-05-06 15:36:46 +0200

    Add: is-subsequence - submission-0

---
+++ Data Structures & Algorithms/is-subsequence/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isSubsequence(s, t) {
+
+let i = 0
+let j = 0
+
+while(i<s.length && j< t.length){
+    if(s[i]===t[j]){
+        i++
+    }j++
+}
+return(i === s.length)
+    }
+}
