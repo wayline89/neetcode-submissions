8ef7b1cc03f2d60bf40d926f40f3712b28ee1978
wayline <44699179+wayline89@users.noreply.github.com>
2026-05-08 10:34:45 +0200

    Add: longest-common-prefix - submission-0

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-0.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    longestCommonPrefix(strs) {
+        let longest = strs[0];
+        for (let mot of strs) {
+            while (!mot.startsWith(longest)) {
+                longest = longest.slice(0, -1);
+                if (longest === "") return "";
+            }
+        }
+        return longest;
+    }
+}
+
