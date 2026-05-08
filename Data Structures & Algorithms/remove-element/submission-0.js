8e04be7e55c5e648f79d0c81902ec9c65e8bf314
wayline <44699179+wayline89@users.noreply.github.com>
2026-05-08 12:15:02 +0200

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} val
+     * @return {number}
+     */
+    removeElement(nums, val) {
+        let k = 0;
+
+        for (let i = 0; i < nums.length; i++) {
+            if(nums[i] !== val){
+            nums[k]= nums[i]
+            k++}
+        }
+        return k
+    }
+}
