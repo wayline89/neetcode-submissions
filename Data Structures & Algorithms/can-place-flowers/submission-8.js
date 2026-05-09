43adb304695729823a50ae0e35ba1820d7bc837c
wayline <44699179+wayline89@users.noreply.github.com>
2026-05-09 22:30:01 +0200

    Add: can-place-flowers - submission-8

---
+++ Data Structures & Algorithms/can-place-flowers/submission-8.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} flowerbed
+     * @param {number} n
+     * @return {boolean}
+     */
+    canPlaceFlowers(flowerbed, n) {
+        let count = 0;
+
+        for (let i = 0; i < flowerbed.length; i++) {
+            if (
+                flowerbed[i] === 0 &&
+                (i === 0 || flowerbed[i - 1] === 0) &&
+                (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)
+            ) {
+                flowerbed[i] = 1;
+                count++;
+                if (count >= n) return true;
+            }
+        }
+        return count >= n;
+    }
+}
